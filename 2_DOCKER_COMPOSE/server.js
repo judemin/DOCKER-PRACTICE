4c97195a6ac94803b7c711e70b237241e72a91c5
judemin <judemin2087@naver.com>
2023-10-23 16:36:38 +0900

    ADD : add 2_DOCKER_COMPOSE

---
+++ 2_DOCKER_COMPOSE/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const redis = require("redis")
+
+// REDIS Client 생성
+const client = redis.createClient({
+    host: "redis-server",
+    port: 6379
+});
+
+const app = express();
+
+// number starts with 0
+app.get('/', async (req, res) => {
+    await client.connect();
+
+    let number = await client.get('number');
+
+    if (number === null)
+        number = 0;
+
+    console.log('Number: ' + number);
+    res.send("숫자가 1씩 올라갑니다. 숫자: " + number);
+
+    await client.set("number", parseInt(number) + 1);
+    await client.disconnect();
+
+});
+
+
+app.listen(8080);
+console.log("Server is Listening : 8080");
